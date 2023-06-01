cc112448259b8f6373d909c143596d0e7bf12643
최현호 <x7788@naver.com>
2023-06-01 16:55:03 +0900

    feat : 메인 화면 최근 게시물 추가

---
+++ src/components/Recent.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { posts } from "@/service/posts";
+import BlogPost from "./BlogPost";
+
+export default function Recent() {
+    return (
+        <div className={`flex flex-col`}>
+            {posts.slice(0.5).map((post) => (
+                <BlogPost
+                    date={post.date}
+                    title={post.title}
+                    des={post.description}
+                    slug={post._raw.flattenedPath}
+                    tag={post.tag}
+                    key={post._id}
+                />
+            ))}
+        </div>
+    );
+}
