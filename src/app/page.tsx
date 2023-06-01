2e167410306eba3c75196a78a62cf2e09fb3d9ad
최현호 <x7788@naver.com>
2023-06-01 12:09:56 +0900

    feat : Hero 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+        <main className="flex min-h-screen flex-col items-center justify-between p-14">
+            <Hero />
+        </main>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,12 @@ import Button from "./Button";
 
 export default function Header() {
     return (
-        <header className="flex justify-between sticky top-0 left-0 z-10 h-20 w-full">
-            <Link href="/" className="flex items-center text-2xl">
+        <header className="flex justify-between sticky top-0 left-0 z-10 h-20 w-full bg-white dark:bg-[#111111]">
+            <Link href="/" className="flex items-center text-2xl font-semibold">
                 HyunHo
             </Link>
-            <nav className="flex items-center gap-4 font-semibold text-xl">
+            <nav className="flex items-center gap-4 text-xl">
                 <Button />
-                <Link href="">About</Link>
                 <Link href="">Resume</Link>
                 <Link href="">Note</Link>
             </nav>
