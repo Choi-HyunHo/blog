5ebe40cfa542d5adabee1a0942e340b01f7c5f28
최현호 <x7788@naver.com>
2023-06-02 21:45:48 +0900

    fix : csr 원인 찾는 중

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState } from "react";
 import BlogPost from "@/components/posts/BlogPost";
 import Tags from "@/components/posts/Tags";
@@ -7,28 +5,28 @@ import Title from "@/components/ui/Title";
 import { posts } from "@/service/posts";
 
 export default function Blog() {
-    const [filteredPosts, setFilteredPosts] = useState(posts); // 필터링된 포스트 목록을 저장하는 상태
+    // const [filteredPosts, setFilteredPosts] = useState(posts); // 필터링된 포스트 목록을 저장하는 상태
 
-    // 태그를 선택하면 해당 태그에 해당하는 포스트만 필터링하여 상태 업데이트
-    const handleFilterByTag = (tag: string) => {
-        const filtered = posts.filter((post) => post.tag === tag);
-        setFilteredPosts(filtered);
-    };
+    // // 태그를 선택하면 해당 태그에 해당하는 포스트만 필터링하여 상태 업데이트
+    // const handleFilterByTag = (tag: string) => {
+    //     const filtered = posts.filter((post) => post.tag === tag);
+    //     setFilteredPosts(filtered);
+    // };
 
-    // 필터링 해제 버튼을 클릭하면 모든 포스트를 보여줌
-    const handleClearFilter = () => {
-        setFilteredPosts(posts);
-    };
+    // // 필터링 해제 버튼을 클릭하면 모든 포스트를 보여줌
+    // const handleClearFilter = () => {
+    //     setFilteredPosts(posts);
+    // };
 
     return (
         <div>
             <Title title="📝 Notes" />
-            <Tags
+            {/* <Tags
                 onFilter={handleFilterByTag}
                 onClearFilter={handleClearFilter}
-            />
+            /> */}
             <div className={`mt-10 flex flex-col`}>
-                {filteredPosts.map((post) => (
+                {posts.map((post) => (
                     <BlogPost
                         date={post.date}
                         title={post.title}
