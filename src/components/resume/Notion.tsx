b7bd9233f0cc30c6b0928ffd3828e7560745a3c0
최현호 <x7788@naver.com>
2023-06-02 23:41:37 +0900

    test : notion 호출

--- src/components/resume/Notion.tsx
+++ src/components/resume/Notion.tsx
@@ -1,13 +1,24 @@
 "use client";
 
+import React, { useState, useEffect } from "react";
 import "react-notion/src/styles.css";
 import { NotionRenderer } from "react-notion";
 
-export default async function Notion() {
-    const res = await fetch(
-        `https://notion-api.splitbee.io/v1/page/${process.env.NEXT_PUBLIC_NOTION_PAGE_ID}`
-    );
-    const data = await res.json();
+export default function Notion() {
+    const [response, setResponse] = useState({});
 
-    return <NotionRenderer blockMap={data} fullPage={true} hideHeader={true} />;
+    useEffect(() => {
+        fetch(
+            `https://notion-api.splitbee.io/v1/page/${process.env.NEXT_PUBLIC_NOTION_PAGE_ID}`,
+            { cache: "force-cache" }
+        )
+            .then((res) => res.json())
+            .then((resJson) => {
+                setResponse(resJson);
+            });
+    }, []);
+
+    return (
+        <NotionRenderer blockMap={response} fullPage={true} hideHeader={true} />
+    );
 }
