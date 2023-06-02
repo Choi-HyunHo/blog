8df775f3a47b7cb649247c99fd70f10e24d80305
최현호 <x7788@naver.com>
2023-06-03 04:20:40 +0900

    refactor : 불필요한 스타일 컴포넌트 제거

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { allPosts } from "contentlayer/generated";
-import Container from "@/components/posts/Container";
 
 export default function Detail({ params }: any) {
     const post = allPosts.find((p) => p._raw.flattenedPath === params.slug);
@@ -13,11 +12,11 @@ export default function Detail({ params }: any) {
     const MDXComponent = useMDXComponent(post.body.code);
 
     return (
-        <Container>
-            <div className="prose max-w-screen-md dark:text-white mt-10 dark:bg-[#121212]">
+        <div className="max-screen-md">
+            <div className="prose  dark:text-white mt-10 dark:bg-[#121212]">
                 <h1>{post.title}</h1>
                 <MDXComponent />
             </div>
-        </Container>
+        </div>
     );
 }
