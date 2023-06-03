8a491e3eb8f190863285676e9f0cce41a177c23a
최현호 <x7788@naver.com>
2023-06-03 17:22:10 +0900

    fix : posts.slice 구문 수정

--- src/components/posts/Recent.tsx
+++ src/components/posts/Recent.tsx
@@ -5,7 +5,7 @@ import BlogPost from "./BlogPost";
 export default function Recent() {
     return (
         <div className={`flex flex-col`}>
-            {posts.slice(0.5).map((post) => (
+            {posts.slice(0, 5).map((post) => (
                 <BlogPost
                     date={post.date}
                     title={post.title}
