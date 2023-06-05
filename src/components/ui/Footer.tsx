366781de41210cac9f38498274898efe8ae41dba
최현호 <x7788@naver.com>
2023-06-05 18:26:39 +0900

    style : footer 텍스트 변경

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer className="w-full font-mono flex flex-col justify-center items-center pt-10 pb-6 transition duration-500">
             <div className="flex justify-center gap-4 items-center pt-4 border-t-2 w-36"></div>
             <div className="text-sm mt-2">Copyright © 2023 HyunHo Choi</div>
-            <div className="text-xs mt-2">HyunHo.dev</div>
+            <div className="text-xs mt-2">choi-hyunho.com</div>
         </footer>
     );
 }
