761a742f720bd04b68bb3daf3ddf175dc4274c1c
최현호 <x7788@naver.com>
2023-06-07 15:24:14 +0900

    refactor : 마크업 변경

--- src/components/posts/BlogPost.tsx
+++ src/components/posts/BlogPost.tsx
@@ -15,18 +15,14 @@ export default function BlogPost({ date, title, des, tag, slug }: Props) {
         <Link href={`/posts/${slug}`} passHref>
             <div className="w-full my-7">
                 <div className="flex gap-2">
-                    <div className="font-medium text-xs text-gray-400">
-                        {date}
-                    </div>
-                    <div className="font-medium text-xs text-gray-400">
-                        {tag}
-                    </div>
+                    <p className="font-medium text-xs text-gray-400">{date}</p>
+                    <p className="font-medium text-xs text-gray-400">{tag}</p>
                 </div>
 
-                <div className={`font-extrabold text-lg md:text-xl mt-2`}>
+                <p className={`font-extrabold text-lg md:text-xl mt-2`}>
                     {title}
-                </div>
-                <div className={`text-lg mt-1`}>{des}</div>
+                </p>
+                <p className={`text-lg mt-1`}>{des}</p>
             </div>
         </Link>
     );
--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
     return (
         <footer className="w-full font-mono flex flex-col justify-center items-center pt-10 pb-6 transition duration-500">
             <div className="flex justify-center gap-4 items-center pt-4 border-t-2 w-36"></div>
-            <div className="text-sm mt-2">Copyright © 2023 HyunHo Choi</div>
-            <div className="text-xs mt-2">choi-hyunho.com</div>
+            <h2 className="text-sm mt-2">Copyright © 2023 HyunHo Choi</h2>
+            <h2 className="text-xs mt-2">choi-hyunho.com</h2>
         </footer>
     );
 }
