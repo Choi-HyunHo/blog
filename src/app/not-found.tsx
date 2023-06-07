993ba2f291f600cb2f92a76205db98808d75df56
최현호 <x7788@naver.com>
2023-06-07 16:49:05 +0900

    feat : custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+import Human from "../../public/images/404.jpg";
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+export default function NotFound() {
+    return (
+        <section>
+            <Image src={Human} alt="404" width={1000} height={100} />
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="mb-2">존재하지 않는 페이지 입니다.</h1>
+                <Link href="/">
+                    <AiOutlineArrowRight className="inline" /> Go Home
+                </Link>
+            </div>
+        </section>
+    );
+}
