fb3b9e181e7c224e65a2cf80e98aabe67050bb30
최현호 <x7788@naver.com>
2023-06-12 10:05:04 +0900

    docs : review 추가

--- src/components/posts/Tags.tsx
+++ src/components/posts/Tags.tsx
@@ -7,7 +7,7 @@ interface Props {
     onClearFilter: () => void;
 }
 
-const tagList = ["All", "Next.js", "React", "TS", "JS", "CSS"]; // 태그 목록 배열
+const tagList = ["All", "Next.js", "React", "TS", "JS", "CSS", "Review"]; // 태그 목록 배열
 
 const Tags = ({ onFilter, onClearFilter }: Props) => {
     const [selectedTag, setSelectedTag] = useState("All"); // 선택된 태그를 저장하는 상태
